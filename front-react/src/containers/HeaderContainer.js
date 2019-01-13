e51b36413fe787ac95aeaad5ffe45c3cd6177a2c
aacc1110 <tadrow@daum.net>
2019-01-13 23:02:11 +0900

    test

---
+++ front-react/src/containers/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header, { LoginButton } from '../components/base/Header';
+
+const HeaderContainer = () => (
+  <Header>
+    <LoginButton />
+  </Header>
+  );
+
+export default HeaderContainer;
